f40c7eff733faaa7974b32f13fa37907e7b12823
anast3t <bortnikov20000@mail.ru>
2024-01-30 13:13:50 +0700

    fix_email -> change_identity

---
+++ change_identity.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+ 
+git filter-branch --env-filter '
+ 
+OLD_EMAIL="wrong_email@example.com"
+NEW_NAME="your_nickname"
+NEW_EMAIL="NEW_email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
